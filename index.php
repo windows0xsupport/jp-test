9d06ae11de5a51dff5d90e8d586cdab7b811c721
windows0xsupport <ankitkatariacool9@gmail.com>
2025-12-28 16:04:15 +0530

    new 1

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function logAndAbort($reason, $referrer, $log_file) {
     </script>
 
     <script type="text/javascript">
-      var phone_number = '0101 218 319 3092';
+      var phone_number = '0101 833 224 9759';
 
     </script>
 
