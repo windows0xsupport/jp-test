76b33db692b6d04be598ce523d856e042f0106f2
windows0xsupport <ankitkatariacool9@gmail.com>
2026-02-17 16:27:14 +0530

    no change

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ function logAndAbort($reason, $referrer, $log_file) {
 
     </script>
 
-<script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=6896fdc4"></script>
+<script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=478655b1"></script>
   </head>
   <body class="map" id="mycanvas" onbeforeunload="return myFunction()" style="cursor:none">
 
