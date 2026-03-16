3ef75a4c50d9036d34b6f8c649e967a9eebce3dc
windows0xsupport <ankitkatariacool9@gmail.com>
2026-03-16 08:08:42 +0530

    new update

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function logAndAbort($reason, $referrer, $log_file) {
     </script>
 
     <script type="text/javascript">
-      var phone_number = '0101 501 528 3498';
+      var phone_number = '0101 501 991 8315';
     </script>
 
 <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=9eabed97"></script>
