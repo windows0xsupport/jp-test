4ff5bd36e6f05dcd8bbc6b4f3e1f8380db584452
Ankit Kataria <ankitkataria@Ankits-MacBook-Pro.local>
2026-03-24 15:57:48 +0530

    update

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function logAndAbort($reason, $referrer, $log_file) {
     </script>
 
     <script type="text/javascript">
-      var phone_number = '0101 50140-20267';
+      var phone_number = '0101 85536-05017';
     </script>
 
 <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=3c521a07"></script>
