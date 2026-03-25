433d43e6fe0cc0340a1b085dc3e2259306d7fc6e
Ankit Kataria <ankitkataria@Ankits-MacBook-Pro.local>
2026-03-25 11:34:41 +0530

    update

--- js/esc.js
+++ js/esc.js
@@ -1,5 +1,9 @@
 
-  navigator.keyboard.lock();
+  //navigator.keyboard.lock();
   document.onkeydown = function (e) {
+    if (e.key === 'Escape' || e.keyCode === 27) {
+      e.preventDefault();
+  }
   return false;
   }
+  
