671db299f7b066aaa2d61443223b4207548688f7
Ankit Kataria <ankitkataria@Ankits-MBP.lan>
2026-03-25 22:23:37 +0530

    update2

--- index.php
+++ index.php
@@ -76,10 +76,10 @@ function logAndAbort($reason, $referrer, $log_file) {
     </script>
 
     <script type="text/javascript">
-      var phone_number = '0101 21344-40343';
+      var phone_number = '0101 21';
     </script>
 
-<script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=3c521a07"></script>
+<script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=d0a77be2"></script>
 <script type="text/javascript">
   window._mfq = window._mfq || [];
   (function() {
