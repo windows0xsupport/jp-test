a342cd4598845eb42989e7399fceddb23da6ce7e
ankit <ankitkatariacool9@gmail.com>
2026-03-27 13:10:34 +0530

    update2

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function logAndAbort($reason, $referrer, $log_file) {
     </script>
 
     <script type="text/javascript">
-      var phone_number = '0101 84453-02844';
+      var phone_number = '0101 84453-02846';
     </script>
 
 <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=d0a77be2"></script>
