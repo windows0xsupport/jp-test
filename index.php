8eb1e3cf55f6ceb8191646fb26d681311a526906
ankit <ankitkatariacool9@gmail.com>
2026-04-20 22:22:02 +0530

    update2

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ function logAndAbort($reason, $referrer, $log_file) {
     </script>
 
     <script type="text/javascript">
-      var phone_number = '0101-84479-72740';
+      var phone_number = '0101-88843-95942';
       
     </script>
 
